683c7f06cd0ec1e948272098a4d0af6c38403db5
ggydo59 <106360843+ggydo59@users.noreply.github.com>
2023-01-05 07:01:40 +0900

    Time: 177 ms (24.80%), Space: 14.6 MB (68.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l1 = len(nums)
-        temp = list(set(nums))
-        temp.sort()
-        l2 = len(temp)
+        j = 0
+        cnt = 1
         
-        for i, value in enumerate(temp):
-            nums[i] = value
-        
-        return l2
+        for i in range(0, len(nums)):
+            if (nums[i] != nums[j]):
+                j += 1
+                nums[j] = nums[i]
+                cnt += 1
+                
+        return cnt
